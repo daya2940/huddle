4059ec29715025b92f60d5048b50acdb8d9d31ee
daya2940 <sauuit2940@gmail.com>
2021-10-30 15:41:16 +0530

    initial Usage of styled component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyledHeader } from "./styles/Header.styles";
+const Header = () => {
+  return (
+    <StyledHeader bg="red">
+      <h1>Header</h1>
+    </StyledHeader>
+  );
+};
+
+export default Header;
---
+++ src/components/styles/Header.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  background-color: #ebfbff;
+  padding: 40px 0;
+`;
+
+// export const StyledHeader = styled.header`
+//   background-color: ${(props) => props.bg};
+//   padding: 40px 0;
+// `;
